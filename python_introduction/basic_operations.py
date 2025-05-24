47634f0afbab6d5fc1c55501813a67776553b537
TmanBE <fadisonolusegun@gmail.com>
2025-05-24 15:05:04 +0100

    python basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,19 @@
+#Assignment of variable
+
+number1 = 10
+number2 = 5
+
+#Addition of the variables
+
+result = number1 + number2
+print("Addition of", number1, "and", number2, "is", result)
+
+#Subtraction of the variables
+
+result = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", result)
+
+#Multiplication of variables
+
+result = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", result)
