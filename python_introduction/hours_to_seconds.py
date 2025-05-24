5004880366ed508f5df3a1662f4b2ebabea933e4
TmanBE <fadisonolusegun@gmail.com>
2025-05-24 18:24:50 +0100

    rectangle_area and hours_to_seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+#Variable definition
+
+hours = 2
+
+#Conversion of hours to seconds
+
+seconds = hours * 3600
+print(hours, "hour(s) is", seconds, "seconds.")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+#Variable definition
+
+length = 10
+width = 5
+
+#Area of a rectangle
+
+area = length * width
+print("The area of the rectangle is:", area)
