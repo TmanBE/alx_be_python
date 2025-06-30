8911224722ce482e67a767fd8f6ef47928662ce9
TmanBE <fadisonolusegun@gmail.com>
2025-06-30 23:15:50 +0100

    Added some bank account operations

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self, account_balance, initial_balance=0):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            return self.account_balance
+        else:
+            raise ValueError("Deposit amount must be positive")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return self.account_balance
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance}")
