e2fe4718f3b92a36b7ab1fd51ce486e0ea193ee3
TmanBE <fadisonolusegun@gmail.com>
2025-07-02 19:48:46 +0100

    some robust division calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,5 +6,5 @@ def safe_divide(numerator, denominator):
             return "Error: Cannot divide by zero."
         else:
             return f"The result of the division is {num / denom}"
-    except ValueError:
-        raise ValueError("Error: Please enter numeric values only.")
+    except (ValueError, TypeError) as e:
+        raise ValueError("Error: Please enter numeric values only.") from e
