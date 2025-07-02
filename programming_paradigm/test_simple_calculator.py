1eb0abed942c2bd7b0867b5c1de3efbaa5259ce3
TmanBE <fadisonolusegun@gmail.com>
2025-07-02 21:08:53 +0100

    Testing my simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,13 +7,15 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assetEqual(self.calc.subtract(-1, 7), -8)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
